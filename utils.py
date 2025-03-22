0a24a4bc15a4dce79d77580f7c5b963c92b3b677
YuxuanZhao <785315305@qq.com>
2025-03-22 16:23:59 -0400

    generate testing data

--- utils.py
+++ utils.py
@@ -1,18 +1,27 @@
 import numpy as np
+from collections import defaultdict, Counter
 
-# Load CSV file (adjust 'filename.csv' to your file's name/path)
-# Assumes the CSV file has no header and columns are separated by commas.
-data = np.loadtxt("news/train_click_log.csv", delimiter=",", dtype=int, skiprows=1)
+test_click_log = np.load('news/test_click_log.npy')
+article_embedding_dict = np.load('news/article_embedding_dict.npy', allow_pickle=True).item()
 
-# Sort the data:
-# We use np.lexsort with a tuple (click_timestamp, user_id).
-# np.lexsort sorts by the last key first, so this sorts by user_id and then by click_timestamp within each user_id.
-sorted_indices = np.lexsort((data[:, 2], data[:, 0]))
-sorted_data = data[sorted_indices]
+test_user_ground_truth = {}
+embeddings = defaultdict(list)
+user_profile = {}
+for i, (user_id, art_id) in enumerate(test_click_log):
+    if i == len(test_click_log) - 1 or test_click_log[i+1][0] != user_id:
+        test_user_ground_truth[int(user_id)] = int(art_id)
+        if user_id not in embeddings:
+            embeddings[int(user_id)].append(article_embedding_dict[art_id])
+    else:
+        embeddings[int(user_id)].append(article_embedding_dict[art_id])
 
-# Extract only the first two columns: user_id and click_article_id
-result = sorted_data[:, :2]
+for user_id, embs in embeddings.items():
+    user_profile[user_id] = np.mean(embs, axis=0)
 
-# Save the result as a .npy file
-np.save("news/train_click_log.npy", result)
+np.save("news/test_user_ground_truth.npy", test_user_ground_truth)
+np.save("news/test_user_profile.npy", user_profile)
 
+# lengths = [len(arr) for arr in embeddings.values()]
+# length_distribution = Counter(lengths)
+# for length, count in sorted(length_distribution.items()):
+#     print(f"Length {length}: {count} keys")
